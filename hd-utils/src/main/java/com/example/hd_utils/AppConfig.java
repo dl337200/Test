0f9b56c99d41ffa13859bdc56b205ff72b8b73cb
hzl090931 <1608304331@qq.com>
2020-12-03 15:47:28 +0800

    添加工具

---
+++ hd-utils/src/main/java/com/example/hd_utils/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.hd_utils;
+
+/**
+ * @author matt
+ * blog: addapp.cn
+ */
+public class AppConfig {
+
+    public static final String DEBUG_TAG = "YF_TAG";// LogCat的标记
+    public static final boolean DEBUG_ENABLE = false;// 是否调试模式
+
+}
